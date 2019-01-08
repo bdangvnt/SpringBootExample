5a58007a15a2803b2ce484c041adc334fd9576ac
bdangvnt@gmail.com <bdangvnt@gmail.com>
2019-01-08 17:32:50 +0700

    08011910

--- src/main/java/com/da/SpringBootExample/controllers/EmployeeController.java
+++ src/main/java/com/da/SpringBootExample/controllers/EmployeeController.java
@@ -12,18 +12,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.da.SpringBootExample.ApplicationConfiguration;
 import com.da.SpringBootExample.models.Employee;
 import com.da.SpringBootExample.repos.EmployeeRepository;
-import com.da.SpringBootExample.services.EmployeeService;
 
 @RestController
 public class EmployeeController {
@@ -60,48 +56,62 @@ public class EmployeeController {
 		}
 	}
 
-//	@RequestMapping(value = "/employee", params = "name", method = GET)
-//	public ResponseEntity<Employee> getByName(@RequestParam String name) {
-//		Employee employee = employeeService.findByName(name);
-//		if (employee.getEmid() == 0) {
-//			System.out.println("Employee Name " + name + " Not Found!");
-//			return new ResponseEntity<Employee>(NOT_FOUND);
-//		} else {
-//			System.out.println("Employee Name " + name + " Found!");
-//			return new ResponseEntity<Employee>(employee, OK);
-//		}
-//	}
-//
-//	@RequestMapping(value = "/addEmployee", method = POST)
-//	public ResponseEntity<Void> addEmployee(@RequestBody Employee employee) {
-//		if (employeeService.addEmployee(employee)) {
-//			System.out.println("Created New Employee!");
-//			return new ResponseEntity<Void>(OK);
-//		} else {
-//			System.out.println("Can not create new employee!");
-//			return new ResponseEntity<Void>(BAD_REQUEST);
-//		}
-//	}
-//
-//	@RequestMapping(value = "/updateEmployee", method = PUT)
-//	public ResponseEntity<Void> updateEmployee(@RequestParam("id") int id, @RequestBody Employee employee) {
-//		if (employeeService.updateEmployee(id, employee)) {
-//			System.out.println("Updated Employee with ID " + id);
-//			return new ResponseEntity<Void>(OK);
-//		} else {
-//			System.out.println("Employee ID " + id + " Not Found!");
-//			return new ResponseEntity<Void>(NOT_FOUND);
-//		}
-//	}
-//
-//	@RequestMapping(value = "/deleteEmployee", method = DELETE)
-//	public ResponseEntity<Void> deleteEmployee(@RequestParam("id") int id) {
-//		if (employeeService.deleteEmployee(id)) {
-//			System.out.println("Deleted Employee with ID " + id);
-//			return new ResponseEntity<Void>(OK);
-//		} else {
-//			System.out.println("Employee ID " + id + " Not Found!");
-//			return new ResponseEntity<Void>(NOT_FOUND);
-//		}
-//	}
+	@RequestMapping(value = "/employee", params = "name", method = GET)
+	public ResponseEntity<Employee> getByName(@RequestParam String name) {
+		Employee employee = employeeRepository.findByName(name).stream().findFirst().get();
+		if (employee.getEmid() == 0) {
+			System.out.println("Employee Name " + name + " Not Found!");
+			return new ResponseEntity<Employee>(NOT_FOUND);
+		} else {
+			System.out.println("Employee Name " + name + " Found!");
+			return new ResponseEntity<Employee>(employee, OK);
+		}
+	}
+
+	@RequestMapping(value = "/employee", params = "namefilter", method = GET)
+	public ResponseEntity<List<Employee>> filterByName(@RequestParam String namefilter) {
+		List<Employee> employees = employeeRepository.findByNameContaining(namefilter);
+		if (employees.isEmpty()) {
+			System.out.println("Employee Name " + namefilter + " Not Found!");
+			return new ResponseEntity<List<Employee>>(NOT_FOUND);
+		} else {
+			System.out.println("Employee Name " + namefilter + " Found!");
+			return new ResponseEntity<List<Employee>>(employees, OK);
+		}
+	}
+
+	@RequestMapping(value = "/addEmployee", method = POST)
+	public ResponseEntity<Void> addEmployee(@RequestBody Employee employee) {
+		if (employeeRepository.save(employee) != null) {
+			System.out.println("Created New Employee!");
+			return new ResponseEntity<Void>(OK);
+		} else {
+			System.out.println("Can not create new employee!");
+			return new ResponseEntity<Void>(BAD_REQUEST);
+		}
+	}
+
+	@RequestMapping(value = "/updateEmployee", method = PUT)
+	public ResponseEntity<Void> updateEmployee(@RequestParam("id") int id, @RequestBody Employee employee) {
+		if (employeeRepository.save(employee) != null) {
+			System.out.println("Updated Employee with ID " + id);
+			return new ResponseEntity<Void>(OK);
+		} else {
+			System.out.println("Employee ID " + id + " Not Found!");
+			return new ResponseEntity<Void>(NOT_FOUND);
+		}
+	}
+
+	@RequestMapping(value = "/deleteEmployee", method = DELETE)
+	public ResponseEntity<Void> deleteEmployee(@RequestParam("id") long id) {
+		employeeRepository.deleteById(id);
+		boolean delres = true;
+		if (delres) {
+			System.out.println("Deleted Employee with ID " + id);
+			return new ResponseEntity<Void>(OK);
+		} else {
+			System.out.println("Employee ID " + id + " Not Found!");
+			return new ResponseEntity<Void>(NOT_FOUND);
+		}
+	}
 }
--- src/main/java/com/da/SpringBootExample/repos/EmployeeRepository.java
+++ src/main/java/com/da/SpringBootExample/repos/EmployeeRepository.java
@@ -1,16 +1,34 @@
 package com.da.SpringBootExample.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.da.SpringBootExample.models.Employee;
 
+//https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+
 @Repository
 @Transactional
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	// @Query("SELECT con FROM Contact con WHERE con.phoneType=(:pType) AND
+	// @Query("SELECT con FROM employee con WHERE con.phoneType=(:pType) AND
 	// con.lastName= (:lName)")
-	// List<Employee> findByLastNameAndPhoneType(@Param("pType") String pType,
-	// @Param("lName") String lName);
+	// List<Employee> findByName(@Param("pType") String pType, @Param("lName")
+	// String lName);
+
+	//@Modifying
+//	@Query("SELECT con FROM employee as con WHERE con.name LIKE %:name%")
+//	List<Employee> filterByName(@Param("name") String name);
+
+	List<Employee> findByNameContaining(@Param("name") String name);
+
+	List<Employee> findByName(@Param("lName") String lName);
+
+	// @Query("SELECT con FROM employee con WHERE con.name LIKE (:name)")
+	// boolean addEmployee(Employee employee);
 }
